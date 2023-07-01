50bff5c885fa953901a84b5f94807dbd8472862c
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-07-01 09:00:35 +0530

    Delete Student Management System directory

--- Student Management System/React/component/HomeComponent.js
+++
@@ -1,9 +0,0 @@
-function HomeComponent(){
-    return(
-        <div>
-            <h1 align="center" color="red" bgcolor="black">Welcome To Hell</h1>
-        </div>
-    )
-}
-
-export default HomeComponent;
